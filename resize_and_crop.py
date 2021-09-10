6df368aea1fd7c0c9784e726d90cd75de2992f41
francisojeah <ojeahfrancisgmail.com>
2021-09-10 18:50:22 +0100

    program to resize and crop images

--- resize_and_crop.py
+++ resize_and_crop.py
@@ -1,6 +1,17 @@
 import cv2
 import numpy as np
-
+#Image is an array of pixels
 img = cv2.imread("Resources/adanna.jpeg")
-cv2.imshow(img)
+#To find the size of image (height, width, number for chanel bgr(3)
+print(img.shape)
+
+#To resize image width by height
+imgResize = cv2.resize(img,(400,500))
+print(imgResize.shape)
+
+#To crop
+imgCropped = img[400:800,400:700]
+#cv2.imshow("Image",img)
+#cv2.imshow("Image Resize", imgResize)
+cv2.imshow("Cropped Resize", imgCropped)
 cv2.waitKey(0)

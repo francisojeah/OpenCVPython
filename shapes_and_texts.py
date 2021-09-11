eb9740a5e61e153f3de17f5e2b377b3a3d1370d3
francisojeah <ojeahfrancisgmail.com>
2021-09-11 10:02:41 +0100

    edited program

--- shapes_and_texts.py
+++ shapes_and_texts.py
@@ -8,8 +8,13 @@ img = np.zeros((512,512,3),np.uint8)
 
 #For lines. image beginning, end, colour, thickness
 cv2.line(img,(0,0),(img.shape[0],img.shape[1]),(0,255,0),3)
-#draw rectangles
-cv2.rectangle(img,(0,0),(350,350),(0,0,255),3)
+#draw rectangles, cv2.FILLED is to fill shape
+cv2.rectangle(img,(0,0),(350,350),(0,0,255),cv2.FILLED)
+#to draw circles
+cv2.circle(img, (400,50),30,(255,255,0),5)
+
+#To put text on images
+cv2.putText(img, "OpenCv",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
 cv2.imshow("Image",img)
 
